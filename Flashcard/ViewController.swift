fb290c66cf35c882a7272e2e63b1d9144c08017b
JessicaOraegbu <81486140+JessicaOraegbu@users.noreply.github.com>
2022-09-22 20:56:25 -0400

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Jessica Oraegbu on 22/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
